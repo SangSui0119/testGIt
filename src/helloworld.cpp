ef5020b673a1751077ac613a68ee42cf6c964ce7
sjh <sephiroth1219@163.com>
2019-11-22 19:28:58 +0800

    create file

---
+++ src/helloworld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : helloworld.cpp
+// Author      : sjh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+		printf("hello git!\n");
+	return 0;
+}
